bfe36da60bd8cd59552b1797124a82048fea7aa3
mwelz <welz@ese.eur.nl>
2021-03-30 18:22:19 +0200

    Fixed computation of confidence intervals for relative risks and rewrote get.benefits(). TODO: Requires an update in the calibration plots

--- funs/estimation-funs.R
+++ funs/estimation-funs.R
@@ -143,60 +143,212 @@ risk.modeling <- function(X, w, y, alpha, offset.lp = TRUE){
 }
 
 
+absolute.observed.benefit <- function(y, w, significance.level = 0.05){
+  # calculates the absolute observed benefit, which corresponds to the difference in mean(y[W=w]), along the associated confidence interval.
+  
+  ttest  <- t.test(y[w==1], y[w==0], paired = FALSE, var.equal = FALSE) 
+  aob    <- unname(ttest$estimate[1] - ttest$estimate[2])
+  se.aob <- unname(ttest$stderr) 
+  
+  # quantile of the standard normal distribution
+  z <- qnorm(1 - significance.level/2)
+  
+  return(c(estimate = aob, 
+           ci.lower = aob - z * se.aob,
+           ci.upper = aob + z * se.aob,
+           stderr   = se.aob))
+}
+
+
+relative.observed.benefit <- function(y, w, significance.level = 0.05){
+  # calculates the relative observed benefit, which corresponds to a risk ratio, along the associated confidence interval. 
+  
+  #' contingency table with the following structure
+  #'      Y=1 Y=0
+  #'  W=1  a   b
+  #'  W=0  c   d
+  a <- sum(w == 1 & y == 1)
+  b <- sum(w == 1 & y == 0)
+  c <- sum(w == 0 & y == 1)
+  d <- sum(w == 0 & y == 0)
+  
+  # estimates for the probabilities
+  p1.hat <- a / (a + b) # estimates Pr(Y = 1 | W = 1)
+  p0.hat <- c / (c + d) # estimates Pr(Y = 1 | W = 0)
+  
+  # empirical risk ratio (the relative observed benefit) and its standard error
+  rob    <- p1.hat / p0.hat
+  se.rob <- sqrt(b / (a * (a + b)) + d / (c * (c + d)))
+  
+  # quantile of the standard normal distribution
+  z <- qnorm(1 - significance.level/2)
+  
+  return(c(estimate = rob, 
+           ci.lower = rob * exp(-z * se.rob),
+           ci.upper = rob * exp(z * se.rob),
+           stderr   = se.rob))
+}
+
+
+odds.ratio <- function(y, w, significance.level = 0.05){
+  # calculates the odds ratio, along the associated confidence interval. 
+  
+  #' contingency table with the following structure
+  #'      Y=1 Y=0
+  #'  W=1  a   b
+  #'  W=0  c   d
+  a <- sum(w == 1 & y == 1)
+  b <- sum(w == 1 & y == 0)
+  c <- sum(w == 0 & y == 1)
+  d <- sum(w == 0 & y == 0)
+  
+  # estimates for the probabilities
+  p1.hat <- a / (a + b) # estimates Pr(Y = 1 | W = 1)
+  p0.hat <- c / (c + d) # estimates Pr(Y = 1 | W = 0)
+  
+  # empirical odds ratio and its standard error
+  or <- (p1.hat / (1 - p1.hat) ) / (p0.hat / (1 - p0.hat))
+  se.or <- sqrt(1/a + 1/b + 1/c + 1/d)
+  
+  # quantile of the standard normal distribution
+  z <- qnorm(1 - significance.level/2)
+  
+  return(c(estimate = or, 
+           ci.lower = or * exp(-z * se.or),
+           ci.upper = or * exp(z * se.or),
+           stderr   = se.or))
+}
+
+
+predicted.benefit <- function(predicted.benefits, significance.level = 0.05){
+  # calculates the predicted benefit, which is, depending on the input, either the relative predicted benefit or the absolute predicted benefit, along the associated confidence interval.
+  
+  ttest <- t.test(predicted.benefits)
+  pb    <- unname(ttest$estimate)
+  se.pb <- unname(ttest$stderr)
+  
+  # quantile of the standard normal distribution
+  z <- qnorm(1 - significance.level/2)
+  
+  return(c(estimate = pb, 
+           ci.lower = pb - z * se.pb,
+           ci.upper = pb + z * se.pb,
+           stderr   = se.pb))
+}
+
+
 get.benefits <- function(pred.model.obj, 
                          cutoffs = c(0.25, 0.5, 0.75),
-                         relative = FALSE){
+                         significance.level = 0.05){
+  
+  # calculates the observed and predicted relative and absolute benefits as well as the odds ratio along the associated confidence intervals.
+  
+  # extract outcome and treatment status
   y <- pred.model.obj$inputs$y
   w <- pred.model.obj$inputs$w
   
   # group observations by their quantile of predicted baseline risk
   quantile.groups <- quantile.group(pred.model.obj$risk.baseline, cutoffs)
   
-  # get predicted benefit (relative or absolute, depends on input)
-  if(relative){
-    pred.ben <- pred.model.obj$predicted.relative.benefit
-  } else{
-    pred.ben <- pred.model.obj$predicted.absolute.benefit
-  }
+  # get predicted benefit (relative and absolute)
+  rel.pred.ben <- pred.model.obj$predicted.relative.benefit
+  abs.pred.ben <- pred.model.obj$predicted.absolute.benefit
   
   ## calculate observed benefit and predicted benefit for each quantile group
   # initialize
-  obs.ben.mat           <- as.data.frame(matrix(NA_real_, ncol(quantile.groups), 4))
-  colnames(obs.ben.mat) <- c("quantile", "mean", "stderr", "df")
-  obs.ben.mat$quantile  <- gsub(" .*$", "", colnames(quantile.groups))
-  pred.ben.mat          <- obs.ben.mat
+  rel.obs.ben.mat           <- as.data.frame(matrix(NA_real_, ncol(quantile.groups), 5))
+  colnames(rel.obs.ben.mat) <- c("quantile", "estimate", "ci.lower", "ci.upper", "stderr")
+  rel.obs.ben.mat$quantile  <- gsub(" .*$", "", colnames(quantile.groups))
+  abs.obs.ben.mat  <- rel.obs.ben.mat
+  abs.pred.ben.mat <- rel.obs.ben.mat
+  rel.pred.ben.mat <- rel.obs.ben.mat
+  or.mat           <- rel.obs.ben.mat
   
   for(i in 1:ncol(quantile.groups)){
     group <- quantile.groups[,i]
     
+    # absolute observed benefit
+    abs.obs.ben.mat[i, 2:5] <- 
+      absolute.observed.benefit(y[group], w[group], significance.level = significance.level)
     
-    if(!relative){
-      ## observed absolute benefit
-      # corresponds to difference in mean(y[group & W=w])
-      ttest                    <- t.test(y[group & w == 1], y[group & w == 0])
-      obs.ben.mat[i, "mean"]   <- unname(ttest$estimate[1] - ttest$estimate[2])
-      obs.ben.mat[i, "stderr"] <- unname(ttest$stderr) 
-      obs.ben.mat[i, "df"]     <- unname(ttest$parameter)
-    } else{
-      ## observed relative benefit
-      ttest                    <- t.test(y[group & w == 1], y[group & w == 0])
-      obs.ben.mat[i, "mean"]   <- mean(y[group & w == 1]) / mean(y[group & w == 0])
-      obs.ben.mat[i, "stderr"] <- unname(ttest$stderr) # TODO: wrong, check how it works!
-      obs.ben.mat[i, "df"]     <- sum(group) - 1  
-    } # IF
+    # absolute predicted benefit
+    abs.pred.ben.mat[i, 2:5] <- 
+      predicted.benefit(abs.pred.ben[group], significance.level = significance.level)
     
-    ## group by predicted benefit
-    pred.ben.mat[i, "mean"]   <- mean(pred.ben[group])
-    pred.ben.mat[i, "stderr"] <- sqrt(var(pred.ben[group]) / sum(group))
-    pred.ben.mat[i, "df"]     <- sum(group) - 1
-  }
+    # relative observed benefit
+    rel.obs.ben.mat[i, 2:5] <- 
+      relative.observed.benefit(y[group], w[group], significance.level = significance.level)
+    
+    # relative predicted benefit
+    rel.pred.ben.mat[i, 2:5] <- 
+      predicted.benefit(rel.pred.ben[group], significance.level = significance.level)
+    
+    # odds ratio
+    or.mat[i, 2:5] <- 
+      odds.ratio(y[group], w[group], significance.level = significance.level)
+    
+  } # FOR
   
-  return(list(
-    group.observed.benefit = obs.ben.mat,
-    group.predicted.benefit = pred.ben.mat,
-    group.membership = quantile.groups
-  ))
+  return(list(absolute.observed.benefit = abs.obs.ben.mat, 
+              absolute.predicted.benefit = abs.pred.ben.mat, 
+              relative.observed.benefit = rel.obs.ben.mat, 
+              relative.predicted.benefit = rel.pred.ben.mat, 
+              odds.ratio = or.mat,
+              significance.level = significance.level,
+              quantile.cutoff.points = cutoffs,
+              group.membership = quantile.groups))
 }
+
+
+get.benefits.grf <- function(grf.model.obj, 
+                             cutoffs = c(0.25, 0.5, 0.75),
+                             significance.level = 0.05){
+  # calculates the observed and predicted absolute benefits along the associated confidence intervals for the GRF (relative effects cannot be computed)
+  
+  # extract outcome and treatment status
+  y <- grf.model.obj$inputs$y
+  w <- grf.model.obj$inputs$w
+  
+  # group observations by their quantile of predicted baseline risk
+  quantile.groups <- quantile.group(grf.model.obj$risk.baseline, cutoffs)
+  
+  ## calculate observed benefit and predicted benefit for each quantile group
+  # initialize
+  abs.obs.ben.mat           <- as.data.frame(matrix(NA_real_, ncol(quantile.groups), 5))
+  colnames(abs.obs.ben.mat) <- c("quantile", "estimate", "ci.lower", "ci.upper", "stderr")
+  abs.obs.ben.mat$quantile  <- gsub(" .*$", "", colnames(quantile.groups))
+  abs.pred.ben.mat <- abs.obs.ben.mat
+  
+  for(i in 1:ncol(quantile.groups)){
+    group <- quantile.groups[,i]
+    
+    ## absolute observed benefit
+    abs.obs.ben.mat[i, 2:5] <- 
+      absolute.observed.benefit(y[group], w[group], significance.level = significance.level)
+    
+    ## absolute predicted benefit
+    ate.group <- grf::average_treatment_effect(grf.model.obj$causal.forest.obj, 
+                                               subset = group)
+    ate <- unname(ate.group["estimate"])
+    se  <- unname(ate.group["std.err"]) 
+    
+    # quantile of the standard normal distribution
+    z <- qnorm(1 - significance.level/2)
+    
+    abs.pred.ben.mat[i, "estimate"] <- ate
+    abs.pred.ben.mat[i, "stderr"]   <- se
+    abs.pred.ben.mat[i, "ci.lower"] <- ate - z * se
+    abs.pred.ben.mat[i, "ci.upper"] <- ate + z * se
+    
+  } # FOR
+  
+  return(list(absolute.observed.benefit = abs.obs.ben.mat, 
+              absolute.predicted.benefit = abs.pred.ben.mat, 
+              significance.level = significance.level,
+              quantile.cutoff.points = cutoffs,
+              group.membership = quantile.groups))
+}
+
 
 
 calibration.plot <- function(pred.model.obj,
@@ -587,50 +739,6 @@ rate.ratio <- function(y, w, lifeyears, subgroup = NULL, ...){
 #' @export
 c.index <- function(y, risk.predictions){
   DescTools::Cstat(x = risk.predictions, resp = y)
-}
-
-get.benefits.grf <- function(grf.model.obj, 
-                             cutoffs = c(0.25, 0.5, 0.75)){
-  # cannot give relative risk!
-  y <- grf.model.obj$inputs$y
-  w <- grf.model.obj$inputs$w
-  
-  # group observations by their quantile of predicted baseline risk (predictions from random forest)
-  quantile.groups <- quantile.group(grf.model.obj$risk.baseline, cutoffs)
-  
-  # get predicted absolute benefit (predictions from causal forest)
-  pred.ben <- grf.model.obj$predicted.absolute.benefit
-  
-  ## calculate observed benefit and predicted benefit for each quantile group
-  # initialize
-  obs.ben.mat           <- as.data.frame(matrix(NA_real_, ncol(quantile.groups), 4))
-  colnames(obs.ben.mat) <- c("quantile", "mean", "stderr", "df")
-  obs.ben.mat$quantile  <- gsub(" .*$", "", colnames(quantile.groups))
-  pred.ben.mat          <- obs.ben.mat
-  
-  for(i in 1:ncol(quantile.groups)){
-    group <- quantile.groups[,i]
-    
-    ## observed absolute benefit (grf cannot do relative benefit)
-    # corresponds to difference in mean(y[group & W=w])
-    ttest                    <- t.test(y[group & w == 1], y[group & w == 0])
-    obs.ben.mat[i, "mean"]   <- unname(ttest$estimate[1] - ttest$estimate[2])
-    obs.ben.mat[i, "stderr"] <- unname(ttest$stderr) 
-    obs.ben.mat[i, "df"]     <- unname(ttest$parameter)
-    
-    ## group by predicted benefit
-    ate.group <- grf::average_treatment_effect(grf.model.obj$causal.forest.obj, 
-                                               subset = group)
-    pred.ben.mat[i, "mean"]   <- unname(ate.group["estimate"])
-    pred.ben.mat[i, "stderr"] <- unname(ate.group["std.err"]) 
-    pred.ben.mat[i, "df"]     <- sum(group) - ncol(grf.model.obj$inputs$X)
-  } # IF
-  
-  return(list(
-    group.observed.benefit = obs.ben.mat,
-    group.predicted.benefit = pred.ben.mat,
-    group.membership = quantile.groups
-  ))
 }
 
 
