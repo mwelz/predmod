c4c737ff09dfb813aa08724559282dd7488bc448
mwelz <welz@ese.eur.nl>
2021-05-16 11:48:43 +0200

    Incomplete draft of plotter

--- funs/generic-ml/gen-ml-test.R
+++ funs/generic-ml/gen-ml-test.R
@@ -1,5 +1,7 @@
 rm(list = ls()) ; cat("\014")
 library(glmnet) # apparently needs to be loaded (?) TODO
+library(ggplot2)
+
 
 source(paste0(getwd(), "/funs/generic-ml/generic-ml-estimation-funs.R"))
 source(paste0(getwd(), "/funs/generic-ml/generic-ml-auxiliary-funs.R"))
@@ -44,10 +46,12 @@ Y  <- ifelse(D == 1, Y1, Y0) # observed outcome
 quantile.cutoffs       = c(0.25, 0.5, 0.75) # for the GATES grouping of S (argument)
 proportion.in.main.set = 0.5 # argument
 Z.clan                 = NULL # argument. The matrix of variables that shall be considered in CLAN
-learners.genericML <- c('glm', 'tree', 'mlr3::lrn("ranger", num.trees = 50)')
-learner.propensity.score <- "glm"
-num.splits <- 4
+learners.genericML <- "glm" #'mlr3::lrn("ranger", num.trees = 50)'# c('glm') #, 'tree', 'mlr3::lrn("ranger", num.trees = 50)') plot below is weird if you uncomment!
+learner.propensity.score <- 'mlr3::lrn("glmnet", lambda = 0, alpha = 1)' # non-penalized logistic regression
+num.splits <- 2
 significance.level <- 0.05
+store.splits <- FALSE
+store.learners <- FALSE
 
 # TODO: The TODOs in genericML()
 genML <- genericML(Z = Z, D = D, Y = Y, 
@@ -57,10 +61,44 @@ genML <- genericML(Z = Z, D = D, Y = Y,
                    Z.clan = Z.clan,
                    quantile.cutoffs = quantile.cutoffs,
                    proportion.in.main.set = proportion.in.main.set, 
-                   significance.level = significance.level)
+                   significance.level = significance.level,
+                   store.splits = store.splits,
+                   store.learners = store.learners)
 
 
 # analyze
 genML$VEIN$best.learners$GATES # difference is insignificant, so no hetero
 genML$VEIN$best.learners$BLP  # beta2 is insignificant, so no hetero
 genML$VEIN$best.learners$CLAN$z1 # there seems to be hetero along z1
+
+
+
+# plot
+df <- data.frame(gates = genML$VEIN$best.learners$GATES[, "Estimate"],
+                 ci.lower = genML$VEIN$best.learners$GATES[, "CI lower"],
+                 ci.upper = genML$VEIN$best.learners$GATES[, "CI upper"],
+                 group = 1:5)
+
+ggplot(mapping = aes(x = group,
+                     y = gates), data = df) +
+  geom_hline(aes(yintercept = genML$VEIN$best.learners$BLP["beta.1", "Estimate"],
+                 color = "ATE"),
+             linetype = "dashed") +
+  geom_hline(aes(yintercept = genML$VEIN$best.learners$BLP["beta.1", "CI lower"],
+                 color = "ATE(90% CB)"),
+             linetype = "dashed")  +
+  geom_hline(yintercept = genML$VEIN$best.learners$BLP["beta.1", "CI upper"],
+             linetype = "dashed", color = "red") +
+  geom_point(aes(color = "GATES with 90% CB"), size = 3) +
+  geom_errorbar(mapping = aes(ymin = ci.lower,
+                              ymax = ci.upper)) +
+  theme_light() +
+  ylab("Treatment Effect") +
+  xlab("Group by HTE Score") +
+  scale_colour_manual(values = c("blue","red", "black")) +
+  theme(legend.title = element_blank(),
+        legend.position = "bottom") + 
+  guides(color = guide_legend(override.aes = list(
+    linetype = 0, size = 4, shape = 15, alpha = 1))
+  )
+  
