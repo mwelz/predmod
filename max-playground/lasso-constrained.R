9751c4c267a85be0f1a7ff2bd5af2f6927168e9c
mwelz <welz@ese.eur.nl>
2021-10-13 20:06:08 +0200

    Added implementation of Lasso as constrained optimization problem

---
+++ max-playground/lasso-constrained.R
@@ -0,0 +1,44 @@
+rm(list = ls()) ; cat("\014")
+
+fr <- function(y, x, betapm) {      
+  
+  p <- length(betapm)/2
+  b <- sapply(1:p, function(j) betapm[j] - betapm[p+j] )
+  
+  as.numeric(crossprod(y - x %*% b))
+}
+
+gr <- function(y, x, betapm){
+  
+  p <- length(betapm)/2
+  xy <- crossprod(x, y)
+  xx <- crossprod(x, x)
+  bp <- betapm[1:p]
+  bn <- betapm[(p+1):(2*p)]
+  
+  as.numeric(
+  rbind(-2 * xy + 2 * xx %*% (bp - bn),
+         2 * xy - 2 * xx %*% (bp - bn)))
+  
+}
+
+
+set.seed(1)
+p <- 10
+x = mvtnorm::rmvnorm(100, mean = rep(0, p), sigma = diag(p))
+beta <-  rep(0, p)
+beta[c(1,3)] <- 2
+y <- as.numeric(x %*% beta + rnorm(100))
+
+t <- sum(abs(beta)) 
+A <- rbind(rep(-1, 2*p),
+           diag(2*p))
+c <- c(-t, rep(0, 2*p))
+
+cop = constrOptim(theta = rep(0.00001, 2*p), f = fr, grad = gr, y = y, x = x, 
+                  ui = A, ci = c)
+
+round(cop$par, 5)
+
+glm <- glmnet::cv.glmnet(x = x, y = y)
+coef(glm)
