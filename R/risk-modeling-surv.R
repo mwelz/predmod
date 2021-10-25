6bb9a2c376be553b4d279f7098621d28bbea82e3
mwelz <welz@ese.eur.nl>
2021-10-25 18:16:57 +0200

    Added Breslow-type cmprsk survival precition function

--- R/risk-modeling-surv.R
+++ R/risk-modeling-surv.R
@@ -34,6 +34,140 @@ survival <- function(time, status, lp, center = FALSE){
   
 } # FUN
 
+
+km_fit <- function(time, status){
+  
+  # status should be binary
+  survival::survfit(survival::Surv(time, status) ~ 1, 
+                    data = data.frame(time, status),
+                    type = "kaplan-meier")
+} # FUN
+
+
+km_predict_survival <- function(x, time){
+  
+  # x is returned by km_fit()
+  summary(x, time = time)$surv
+  
+} # FUN
+
+
+#' creates helper object for predictions in Fine-Gray models
+#' 
+#' @param time time at risk
+#' @param status Status. Zero if survived
+#' @param k Status of interest. Default is 1.
+#' @return A list
+#' 
+#' @export
+prep_predict <- function(time, status, k = 1){
+  
+  # get Kaplan-Meier (KM) estimator of censoring time by using {time, 1 - delta}
+  km <- km_fit(time = time, status = 1*(status == 0))
+  
+  # evaluate KM estimator at all times
+  G <- sapply(1:length(time), function(i) {
+    km_predict_survival(x = km, time = time[i])
+  })
+  
+  # get index set Ik
+  Ik <- which(status == k)
+  
+  # get Rk_list
+  Rk_list <- 
+    lapply(Ik, function(i){
+      
+      # get set R_{i,k}
+      Rk <- which(time >= time[i] | (0 < status & status != k))
+      
+      # get Z_j(Y_i) * w_j(Y_i) for all j \in R_{i,k}
+      zw <- sapply(Rk, function(j){
+        ifelse(test = (time[j] < time[i]) & (0 < status[j]) & (status[j] != k), 
+               yes = G[i] / G[j], no = 1.0)
+      })
+      
+      # return
+      list(Rk = Rk, zw = zw)
+      
+    }) # LAPPLY
+  
+  # naming
+  names(Rk_list) <- paste0("idx", Ik)
+  
+  # return
+  return(list(Ik = Ik, Rk_list = Rk_list, G = G, KM = km))
+  
+} # FUN
+
+
+# estimates Fine-Gray Breslow baseline hazard function
+basehaz_cmprsk <- function(time, status, lp, time_eval, prep_predict_object = NULL, k = 1){
+  
+  # we can only evaluate at one time (for simplicity)
+  stopifnot(length(time_eval) == 1)
+  
+  # prepare prediction
+  if(is.null(prep_predict_object)){
+    prep <- prep_predict(time = time, status = status, k = k)
+  } else{
+    prep <- prep_predict_object
+  }
+  
+  # get Rk_list and Ik
+  Ik <- prep$Ik
+  Rk_list <- prep$Rk_list
+  
+  # get w_i(T_i)*N_{i,k}(t) for all i
+  wn <- 1 * (time <= time_eval)
+  wn[setdiff(1:length(time), Ik)] <- NA_real_ # unobserved at these times! 
+  
+  # get exp(lp) on all samples
+  explp <- exp(as.numeric(lp))
+  
+  # predict baseline survival 
+  sum(sapply(Ik, function(i){
+    
+    zw  <- Rk_list[[paste0("idx",i)]]$zw
+    Rk  <- Rk_list[[paste0("idx",i)]]$Rk
+    wn[i] / sum(zw * explp[Rk])
+    
+  })) # SAPPLY
+} # FUN
+
+#' estimates survival in subdistributional hazard models
+#' 
+#' @param time A vector of times that was used to fit a cmprsk model
+#' @param status A vector of mortality status that was used to fit a cmprsk model
+#' @param lp A linear predictor, obtained from a cmprsk model
+#' @param k The failure typr of interest that was used to fit a cmprsk model
+#' @param prep_predict_object Output of \code{\link{prep_predict}}.
+#' 
+#' @noRd
+survival_cmprsk <- function(time, status, lp, prep_predict_object = NULL, k = 1){
+  
+  # Fine-Gray Breslow baseline survival function
+  basesurv <- function(time.eval){
+    
+    H0k <- basehaz_cmprsk(time = time, status = status, lp = lp, 
+                          time_eval = time.eval, 
+                          prep_predict_object = prep_predict_object, k = k)
+    exp(-H0k) 
+    
+  } # FUN
+  
+  # function that returns survival probability at cause k
+  surv <- function(time.eval){
+    
+    basesurv(time.eval = time.eval)^exp(lp)
+    
+  } # FUN
+  
+  # return
+  return(list(basesurv = basesurv, surv = surv))
+  
+} # FUN
+
+
 # TODO: In baseline.risk of risk.modeling, there is error in coef: no var.kept there when naming the variables! The @x is wrong, as only nonzero gets printed!
 # TODO: email Trevor about cryptic error in predict and coef.obj@i+1
 # TODO: center argument
