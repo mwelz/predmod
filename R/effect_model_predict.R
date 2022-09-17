3f0d8b30758b45fe3d5201ac7cb581a7744e07be
mwelz <welz@ese.eur.nl>
2022-09-17 20:27:32 +0200

    added 'shrunk' argument to effect model prediction

--- R/effect_model_predict.R
+++ R/effect_model_predict.R
@@ -3,6 +3,7 @@
 #' @param object A \code{effect_model_crss} object.
 #' @param newX A numeric matrix at which predictions should be performed
 #' @param neww Treatment assignment
+#' @param shrunk Shall 1st (TRUE) or 2nd (FALSE) stage be used for predictions?
 #' @param ... Additional parameters to be passed down
 #' 
 #' @return A matrix of risk predictions
@@ -11,6 +12,7 @@
 predict.effect_model <- function(object,
                                  neww, 
                                  newX,
+                                 shrunk = FALSE,
                                  ...)
 {
   
@@ -34,6 +36,7 @@ predict.effect_model <- function(object,
   predict_effect_model_NoChecks(object = object,
                                 neww = neww, 
                                 newX = newX,
+                                shrunk = shrunk,
                                  ... = ...)
   
 } # FUN
@@ -43,6 +46,7 @@ predict.effect_model <- function(object,
 predict_effect_model_NoChecks <- function(object,
                                           neww, 
                                           newX,
+                                          shrunk,
                                           ...)
 {
   
@@ -74,7 +78,7 @@ predict_effect_model_NoChecks <- function(object,
   
   
   ## get the retained variables
-  retained <- rownames(object$coefficients$reduced)
+  retained <- rownames(object$coefficients$reduced) # gives us coefs with nonzero 1st stage estimates
   
   if("(Intercept)" %in% retained)
   {
@@ -93,17 +97,21 @@ predict_effect_model_NoChecks <- function(object,
     # case 2: at least one variable was retained
     X_full0 <- X_full[,retained0, drop = FALSE]
     X_full_rev0 <- X_full_rev[,retained0,drop = FALSE]
-    
     colnames(X_full0) <- colnames(X_full_rev0) <- retained0
     
+    # get coefficients
+    mod_type <- ifelse(shrunk, "full", "reduced")
+    cf <- as.numeric(object$coefficients[[mod_type]][retained, "Estimate"])
+
+
     ## predict risk with regular w... 
     risk_reg <- as.numeric(
-      stats::plogis(cbind(1.0, X_full0) %*% object$models$reduced$coefficients)
+      stats::plogis(cbind(1.0, X_full0) %*% cf)
     )
     
     ## ... and with flipped w
     risk_rev <- as.numeric(
-      stats::plogis(cbind(1.0, X_full_rev0) %*% object$models$reduced$coefficients)
+      stats::plogis(cbind(1.0, X_full_rev0) %*% cf)
     )
     
   } # IF
